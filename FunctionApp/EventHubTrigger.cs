daf0aaf25dee5a48ce679aba822d8211f8482986
Jeff Hollan <jehollan@microsoft.com>
2018-06-11 15:21:56 +0200

    added unit tests for all functions

--- FunctionApp/EventHubTrigger.cs
+++ FunctionApp/EventHubTrigger.cs
@@ -2,6 +2,7 @@ using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.Extensions.Logging;
 using System.Text;
 
 namespace FunctionApp
@@ -9,11 +10,11 @@ namespace FunctionApp
     public static class EventHubTrigger
     {
         [FunctionName("EventHubTrigger")]
-        public static void Run([EventHubTrigger("test", Connection = "EventHubsConnectionString")]EventData[] eventDataArray, TraceWriter log)
+        public static void Run([EventHubTrigger("test", Connection = "EventHubsConnectionString")]EventData[] eventDataArray, ILogger log)
         {
             foreach(var e in eventDataArray)
             {
-                log.Info($"C# Event Hub trigger function processed a message: {Encoding.UTF8.GetString(e.Body.Array)}");
+                log.LogInformation($"C# Event Hub trigger function processed a message: {Encoding.UTF8.GetString(e.Body.Array)}");
             }
         }
     }
